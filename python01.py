7e4a70e4a1d35d2469ce60075920d9ad6b841a27
Patrick Arinelli <arinpar@gmail.com>
2022-02-26 20:47:58 -0300

    Initial commit

---
+++ python01.py
@@ -0,0 +1,100 @@
+
+
+
+name = "Patrick Arinelli"
+print(name)
+
+# Parte 1
+print ("\n----------Parte 1----------")
+
+(n_2) = 5 + 2j
+(n_3) =  5 + 3j
+x = (n_2) + (n_3)
+print(x)
+
+# Parte 2
+print("\n----------Parte 2----------")
+
+name2 = "Rodrigues"
+print(name2)
+
+# Parte 3
+print("\n----------Parte 3----------")
+
+lis1 = [4,5,6,7]
+print(lis1[0])
+lis2 = [1,2,3]
+print(lis1[0],lis2[0])
+lis3 = [8,9]
+len(lis3)
+print(len(lis3))
+
+# Parte 4
+print("\n----------Parte 4----------")
+
+lis4 = ["P","Y","T","H","O","N"]
+lis5 = ["python"]
+print(lis5[0])
+print(lis4[:2])
+print ("".join(map(str,lis4)))
+print(lis5[0])
+print(lis4[:4])
+print("" .join(map(str,lis4[:4])))
+
+# Parte 5
+print("\n----------Parte 5----------")
+
+x = -30
+
+if x > 0:
+    print("X é positivo")
+elif x < 0:
+    print("X é negativo")
+else:
+    print("X é igual a ele mesmo")
+
+x = 5
+
+if x is not str:
+    print(x)
+
+y = "É uma string"
+if type(y) is str:
+    print(y)
+
+z = 20
+if z > 10 and z < 20:
+    print("Z é maior que 10 e é menor que 20")
+elif z > 10 and z < 22:
+    print("Z é maior que 10 E é menor que 22")
+
+b = 15
+if b == 10 or b > 15:
+    print("b é igual a 10 ou b é maior que 15")
+elif b < 15 or b == 14:
+    print("b é maior que 5 ou b é menor que 12")
+else:
+    print("b não atende à nenhuma das possibilidades acima")
+
+# Parte 6
+print("\n----------Parte 6----------")
+
+lis10 = [0,"w",4,5,6,"z"]
+
+for i in enumerate(lis10):
+    print(i)
+
+
+# Parte 7
+print("\n----------Parte 7----------")
+
+
+
+
+
+
+
+
+
+
+
